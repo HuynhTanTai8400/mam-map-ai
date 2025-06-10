e402f34530d41b8253ad28b3d9010e00986e7159
HuynhTai488 <htai8400@gmail.com>
2025-06-10 17:12:54 +0700

    fix port lan 2

--- app.py
+++ app.py
@@ -110,5 +110,4 @@ def predict():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ["PORT"])  
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
